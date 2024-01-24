d621ec5ebed4a5f09f2dee43737f513d8cbd68e9
AlexisGado <ipod.alexisgado@gmail.com>
2024-01-24 01:54:02 +0100

    global css reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,6 @@
 	:root {
 		--red-bg: #801b22;
 	}
-	:global(body) {
-		margin: 0;
-		padding: 0;
-	}
-	:global(h1) {
-		margin: 0;
-	}
 	.cool-bg {
 		background: repeating-linear-gradient(
 			100deg,
